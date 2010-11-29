b961b4e85960ff473ced09f68d6e595bd92f3a19
Shawn M Moore <sartak@bestpractical.com>
2010-11-29 12:59:41 -0500

    Doc updates

--- lib/Log/Dispatch/Twitter.pm
+++ lib/Log/Dispatch/Twitter.pm
@@ -71,10 +71,16 @@ Log::Dispatch::Twitter - Log messages via Twitter
     my $logger = Log::Dispatch->new;
 
     $logger->add(Log::Dispatch::Twitter->new(
+
         username  => "foo",
         password  => "bar",
-        min_level => "debug",
-        name      => "twitter",
+
+        # Net::Twitter args
+        traits              => [qw/OAuth API::REST/],
+        consumer_key        => $consumer_key,
+        consumer_secret     => $consumer_secret,
+        access_token        => $token,
+        access_token_secret => $token_secret,
     ));
 
     $logger->log(
@@ -92,7 +98,7 @@ Shawn M Moore, C<sartak@gmail.com>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright 2008-2009 Shawn M Moore.
+Copyright 2008-2010 Shawn M Moore.
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
